fca2310dc00d83175c428890605b313094ba9e89
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2020-11-26 08:14:39 +0800

    Update test.php

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -2,16 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Test extends CI_Controller {
-
-
-    
-    public function __construct()
-    {
-        parent::__construct();
-        //Do your magic here
-    }
-    
+class test extends CI_Controller {
 
     public function index()
     {
